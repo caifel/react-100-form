db962abfb14ad593a0c458a26d690d51384ca08b
Mario Medrano (Contractor) <mmedrano@sjcmac1qnml85.paloaltonetworks.local>
2021-02-21 06:17:23 -0800

    Update workflow

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SampleForm from './package/samples';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <SampleForm />
+            </header>
+        </div>
+    );
+}
+
+export default App;
